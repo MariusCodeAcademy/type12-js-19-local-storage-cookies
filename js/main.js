920886c58e83625a018f4a0af19095ddef310aed
Marius <marius@example.com>
2022-08-01 15:54:14 +0300

    cookie start

--- js/main.js
+++ js/main.js
@@ -7,6 +7,7 @@ const emailEl = document.getElementById('email');
 const passwordEl = document.getElementById('password');
 const resultEl = document.getElementById('result');
 const resultEmailEl = document.getElementById('login-email');
+const logoutBtnEl = document.getElementById('logout');
 
 // sustabdyti forma su js
 formEl.addEventListener('submit', (event) => {
@@ -23,6 +24,9 @@ formEl.addEventListener('submit', (event) => {
   // localStorage.setItem(key, value)
   // issaugosim username localStorage
   localStorage.setItem('currentUser', emailVal);
+
+  hideForm();
+  showLoggedIn();
 });
 
 // load user from storage
@@ -31,3 +35,39 @@ console.log('userFromStorage ===', userFromStorage);
 if (userFromStorage !== null) {
   resultEmailEl.textContent = userFromStorage;
 }
+
+/**
+ * checks if there is a user in localstorage
+ * if there is returns user
+ * if not returns false
+ */
+function isUserLoggedIn() {
+  const userFromStorage = localStorage.getItem('currentUser');
+
+  if (userFromStorage !== null) {
+    return userFromStorage;
+  }
+  return false;
+}
+
+function hideForm() {
+  // hide form
+  formEl.classList.add('hide');
+}
+
+function showLoggedIn() {
+  // show success
+  resultEl.classList.remove('hide');
+}
+
+// slepti login forma jei turim user localstorage
+if (isUserLoggedIn()) {
+  hideForm();
+  showLoggedIn();
+}
+
+logoutBtnEl.addEventListener('click', () => {
+  console.log('paspaudei logout');
+  localStorage.removeItem('currentUser');
+  // window.location.reload();
+});
