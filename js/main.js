bdbc518db9b1da047ab81d8c39adfdfd06e70618
Marius <marius@example.com>
2022-08-01 14:35:05 +0300

    append

--- js/main.js
+++ js/main.js
@@ -3,17 +3,22 @@ console.log('main.js');
 
 // taikomes
 const formEl = document.forms[0];
+const emailEl = document.getElementById('email');
+const passwordEl = document.getElementById('password');
+const resultEl = document.getElementById('result');
 
 // sustabdyti forma su js
 formEl.addEventListener('submit', (event) => {
   event.preventDefault();
   console.log('Js controll the form');
+  // paimti abu input reiksmes
+  const emailVal = emailEl.value;
+  const passwordVal = passwordEl.value;
+  console.log({ emailVal, passwordVal });
+  // isvaly inputus
+  formEl.reset();
+  // atvaizduoti prisijungusio zmogaus username/email htmle
+  resultEl.firstElementChild.append(emailVal);
 });
-
-// paimti abu input reiksmes
-
-// isvaly inputus
-
-// atvaizduoti prisijungusio zmogaus username/email htmle
 
 // issaugosim username localStorage
