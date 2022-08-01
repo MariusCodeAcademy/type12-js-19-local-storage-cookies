f90730a9d8d4a0469412de146129d7325d109385
Marius <marius@example.com>
2022-08-01 14:20:38 +0300

    js controll form

--- js/main.js
+++ js/main.js
@@ -1,10 +1,19 @@
 'use strict';
 console.log('main.js');
 
+// taikomes
+const formEl = document.forms[0];
+
 // sustabdyti forma su js
+formEl.addEventListener('submit', (event) => {
+  event.preventDefault();
+  console.log('Js controll the form');
+});
 
 // paimti abu input reiksmes
 
 // isvaly inputus
+
+// atvaizduoti prisijungusio zmogaus username/email htmle
 
 // issaugosim username localStorage
